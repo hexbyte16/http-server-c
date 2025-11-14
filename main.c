7bcf719d7da4f0b644ddf8c6a8fecf83462a1435
Hexbyte <islamamara062@gmail.com>
2025-11-14 22:22:33 +0000

    Made a to do list app using c

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
+// array_fail.c
 #include <stdio.h>
+#include <string.h>
+
+int main() {
+
+  char todos[5][50];
+
+  int counter = 0;
+
+  strcpy(todos[counter++], "Learn C");
+  strcpy(todos[counter++], "Build HTTP server");
+  strcpy(todos[counter++], "Master pointers");
+
+  printf("Enter a new task: \n");
+  fgets(todos[counter], 49, stdin);
+  counter++;
+
+  for (int i = 0; i < counter; i++) {
+    printf("%d. %s.\n", i + 1, todos[i]);
+  };
+
+  return 0;
+}
